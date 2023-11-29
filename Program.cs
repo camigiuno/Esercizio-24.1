d122bdc393b04a824fd535d148889cf8b47c5b28
paolo <paolo@LAPTOP-PFR3VSID>
2023-11-29 21:29:13 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Esercizio_24_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrato l = new Quadrato();
+            Console.WriteLine("Inserisci il lato del quadrato");
+            l.Lato= float.Parse(Console.ReadLine());
+            Console.WriteLine("L'area del quadrato è:", l.Lato*l.Lato);
+            Console.Read();
+        }
+    }
+}
